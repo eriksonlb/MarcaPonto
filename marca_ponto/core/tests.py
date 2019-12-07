652ff4658837f3017b85a6faa64eeeb15ee651ad
eriksonlb <erikson.lopes@live.com>
2019-12-07 06:16:24 -0300

    testes

--- marca_ponto/core/tests.py
+++ marca_ponto/core/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import time
 
-# Create your tests here.
+driver = webdriver.Firefox()
+
+url = 'http://127.0.0.1:8000/'
+username = 'username' #id
+password = 'pwd' #id
+btn_logar = 'logar' #name
+  
+def navigate():
+  driver.get(url)
+
+
+def login(username='qa_tester', password='abacatecomarroz2323'):
+  driver.find_element_by_id(username).send_keys(username)
+  driver.find_element_by_id(password).send_keys(password)
+  driver.find_element_by_name(btn_logar).click()
+
+navigate()
+time.sleep(5)
+login()
+
--- marca_ponto/marca_ponto/settings.py
+++ marca_ponto/marca_ponto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=$%g$#ls(*td)@8xo$k%lc$=gwr0gxbywm4u9)ugvtjz!(-d0r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
